d6cbed44777d118605913c1f6d9100dfd4cc35c3
Gallo Giovanni Donato <g.gallo27@campus.unimib.it>
2019-10-23 10:15:19 +0200

    Aggiunto il metodo assertGreaterThan(T, T, Comparator<T>) alla classe org.util.Assert

--- src/main/java/org/junit/Assert.java
+++ src/main/java/org/junit/Assert.java
@@ -955,4 +955,8 @@ public class Assert {
             Matcher<? super T> matcher) {
         MatcherAssert.assertThat(reason, actual, matcher);
     }
+    
+    public static <T> void assertGreaterThan(T o1, T o2, java.util.Comparator<T> comparator) {
+        assertTrue(comparator.compare(o1, o2)>0);           
+    }
 }
